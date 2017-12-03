6c7e7f84570dde9503d1299f14173b80dd54a4ec
Eric Carles <carles.eric@wanadoo.fr>
2017-12-03 17:52:29 +0000

    TestPlaylistManagement refcatoring of Initialize method

--- play_list_management.rb
+++ play_list_management.rb
@@ -4,8 +4,7 @@ class PlayListManagement
 
   attr_reader  :play_list
 
-  def initialize(booking, songs_catalog)
-    @booking         = booking
+  def initialize(songs_catalog)
     @songs_catalog   = songs_catalog
     @play_list       = Array.new()
   end
@@ -18,13 +17,8 @@ class PlayListManagement
     return @songs_catalog.find{|song| song.title == title} != nil
   end
 
-  def is_enough_time_left?(song_duration)
-    return song_duration <= (@booking.end_time - Time.now())/60
-  end
-
   def control_adding_new_song(song)
     return false if ! song_available?(song.title)
-    return false if ! is_enough_time_left?(song.duration)
     add_song_to_play_list(song)
     return true
   end
--- specs/play_list_management_specs.rb
+++ specs/play_list_management_specs.rb
@@ -11,19 +11,11 @@ class TestPlaylistManagement< Minitest::Test
 
   def setup
 
-    @guest1   = Guest.new("Bruce", 100)
-    @guest2   = Guest.new("Ashley", 120)
-    @guest3   = Guest.new("Vince", 60)
-    @guests_payment = [GuestPayment.new(@guest1,10), GuestPayment.new(@guest2,15), GuestPayment.new(@guest3,15)]
-
-    @room    = Room.new("BlueRoom", 10, 20)
-    @booking = Booking.new(@room, Time.now(), 2, @guests_payment)
-
     @song1 = Song.new("One", "U2", Song::MUSIC_KIND[:POP_ROCK], 5)
     @song2 = Song.new("Don't leave me", "PINK", Song::MUSIC_KIND[:POP], 4)
     @song3 = Song.new("Stairway to heaven", "Led Zep", Song::MUSIC_KIND[:ROCK], 121)
     @songs_catalog = [@song1, @song2]
-    @play_list_manegement = PlayListManagement.new(@booking, @songs_catalog)
+    @play_list_manegement = PlayListManagement.new(@songs_catalog)
 
 
   end
@@ -44,14 +36,6 @@ class TestPlaylistManagement< Minitest::Test
     assert_equal(true, @play_list_manegement.song_available?(title))
   end
 
-  def test_is_enough_time_left__false
-    assert_equal(false, @play_list_manegement.is_enough_time_left?(121))
-  end
-
-  def test_is_enough_time_left__true
-    assert_equal(true, @play_list_manegement.is_enough_time_left?(119))
-  end
-
   def test_control_adding_new_song__false
     nb_songs = @play_list_manegement.play_list.count()
     @play_list_manegement.control_adding_new_song(@song3)
@@ -62,6 +46,10 @@ class TestPlaylistManagement< Minitest::Test
     nb_songs = @play_list_manegement.play_list.count()
     @play_list_manegement.control_adding_new_song(@song1)
     assert_equal(nb_songs+1, @play_list_manegement.play_list.count())
+  end
+
+  def test_is_favorite_song_being_played
+
   end
 
 end
